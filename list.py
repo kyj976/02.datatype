378dba5d170c5afb8aceeed65d40eacdd7410597
kyj976 <kyj976@gmail.com>
2024-07-02 16:17:29 +0900

    변수
    변수리스트, 불린, 딕셔너리 등

---
+++ list.py
@@ -0,0 +1,28 @@
+yongjun = ["김용준", '28', '010-4042-6418']
+name = yongjun[0]
+age = yongjun[1]
+phone = yongjun[2]
+print(type(yongjun))
+print(name, age, phone)
+
+
+names = [['강수경', '강혜나', '김민석'], ['20', '21', '22'], ['010-****-****', '12121', '3232']]
+print(names[0:2][0:2])
+
+numbers = [1,2,3,4,5]
+result = numbers[2] + numbers[-1]
+print(result)
+
+print(len(names))
+
+# 리스트에 요소 추가 및 제거하기
+last = [1,2,3]
+last.append(30)
+print(last)
+last.remove(3)
+print(last)
+
+
+
+
+
---
+++ string.py
@@ -0,0 +1,19 @@
+name = '김용준'
+age = '28'
+
+print("-"*40)
+c = name + '님 반갑습니다.' + '나이는' + age + '살 이시군요.'
+print(c)
+print("-"*40)
+
+# 파이썬 데이터의 길이 구하는 함수
+len(c)
+print(len(c))
+
+# 파이썬 문자열의 인덱싱 및 슬라이싱
+str = "smart안전학과 강사 7월 강의 현황"
+smart = str[0:5]
+print(str[-1])
+
+
+
