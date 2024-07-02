e4e09574476a4b2e0be9f5c17cb2ca23eb4f90b2
kyj976 <kyj976@gmail.com>
2024-07-02 17:21:04 +0900

    퀴즈
    퀴즈

---
+++ quiz.py
@@ -0,0 +1,14 @@
+person = {'이름': '김용준', '나이': 28, '이메일주소': 'kyj976@gmail.com', '연락처': '010-4042-6418'}
+
+name = input('이름을 입력하세요.')
+age = input('나이를 입력하세요')
+email = input('이메일 주소를 입력하세요.')
+phone = input('연락처를 입력하세요')
+
+dic = {name: [age, email, phone]}
+print(dic)
+
+
+
+
+
